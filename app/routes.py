7b66988a3c2850270bf3f3292bd43c262d385ee3
Rostislav Dolgushevskiy <rosdol246@gmail.com>
2021-06-08 22:57:31 +0300

    Fixed error when reusing token

--- app/routes.py
+++ app/routes.py
@@ -8,15 +8,19 @@ def index():
     form = ShortedLinkForm()
     if form.validate_on_submit():
         shorted_link = ShortedLink(long_url=form.long_url.data)
-        db.session.add(shorted_link)
-        db.session.commit()
-        shorted_link.set_token(form.token.data) # Two commits because it's takes id to generate token
-        db.session.commit()
-        flash('http://127.0.0.1:5000/' + shorted_link.token)
+        token = form.token.data
+        if ShortedLink.query.filter_by(token=token).first() is None:
+            db.session.add(shorted_link)
+            db.session.commit()
+            shorted_link.set_token(token) # Two commits because it's takes id to generate token
+            db.session.commit()
+            flash('http://127.0.0.1:5000/' + shorted_link.token)
+        else:
+            flash('Use another custom URL')
     return render_template('index.html', form=form)
 
 
 @app.route('/<token>')
-def bro(token):
+def token_redirect(token):
     shorted_link = ShortedLink.query.filter_by(token=token).first()
     return redirect(shorted_link.long_url)
